75fb676c9f2be2709720ae0ffca19f4342f738f8
Isa Bakhtov <issab79@icloud.com>
2022-11-26 18:06:26 +0600

    Добавлены ограничения по правам удаления фото и редактирования фото

--- source/photos/views/photo_delete_view.py
+++ source/photos/views/photo_delete_view.py
@@ -1,13 +1,22 @@
 from django.views.generic import DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.urls import reverse_lazy
 
 
 from photos.models import Photo
 
+# class GroupPermission(UserPassesTestMixin):
+#     groups = []
+    
+#     def test_func(self):
+#         return self.request.user.groups.filter(name__in=self.groups).exists()
 
-
-class PhotoDeleteView(LoginRequiredMixin, DeleteView):
+class PhotoDeleteView(PermissionRequiredMixin,LoginRequiredMixin, DeleteView):
     template_name = 'photo_confirm_delete.html'
     model = Photo
     success_url = reverse_lazy('index')
+    # groups = ['admin', "user"]
+    permission_required = 'photos.can_delete_photo'
+    
+    def has_permission(self):
+        return super().has_permission() or self.get_object().author == self.request.user
--- source/photos/views/photo_update_view.py
+++ source/photos/views/photo_update_view.py
@@ -1,25 +1,31 @@
 from django.views.generic import UpdateView
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin, UserPassesTestMixin
 from django.urls import reverse
 
 from photos.forms import PhotoForm
 from photos.models import Photo
 
 
-class GroupPermission(UserPassesTestMixin):
-    groups = []
+# class GroupPermission(UserPassesTestMixin):
+#     groups = []
     
-    def test_func(self):
-        return self.request.user.groups.filter(name__in=self.groups).exists()
+#     def test_func(self):
+#         return self.request.user.groups.filter(name__in=self.groups).exists()
 
-class PhotoUpdateView(LoginRequiredMixin, UpdateView):
+class PhotoUpdateView(PermissionRequiredMixin,LoginRequiredMixin, UpdateView):
 
     template_name = 'photo_update.html'
     form_class = PhotoForm
     model = Photo
     context_object_name = 'photo'
-    groups = ['admin', 'user']  
+    # groups = ['admin', 'user'] 
+    permission_required = 'photos.can_change_photo'
     
+    
+    def has_permission(self):
+        return super().has_permission() or self.get_object().author == self.request.user
     
     def get_success_url(self):
         return reverse('photo_detail', kwargs={'pk': self.object.pk})
+    
+    
