bbd68c2dc343a70d348bca2fb60c45fbcf45bc10
Isa Bakhtov <issab79@icloud.com>
2022-11-26 18:53:09 +0600

    НЕкоторые улучсения

---
+++ README.txt
@@ -0,0 +1,12 @@
+Суперпользователь
+
+логин - пароль
+
+admin@gmail.com - root
+
+Юзеры
+
+логин - пароль
+
+isa@gmail.com - root
+musa@gmail.com - root
--- source/accounts/views/acc_profile_view.py
+++ source/accounts/views/acc_profile_view.py
@@ -3,7 +3,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
 
 from django.views.generic import  DetailView
-from photos.models import Photo
+from photos.models import Photo, Choice
 # from accounts.forms import SubsForm
 
 
@@ -14,15 +14,29 @@ class ProfileView(LoginRequiredMixin, DetailView):
     context_object_name = 'user_obj'
     paginate_related_by = 5
     paginate_related_orphans = 0
-
+        
+        
     def get_context_data(self,object_list=None, **kwargs):
         context = super(ProfileView, self).get_context_data(object_list=object_list, **kwargs)
         author = self.get_object()
-        print("++++++", author, "+++++++",self.request.user, "=======", context, "++++++", self.get_object())
-        photos = Photo.objects.filter(author=author).order_by('-created_at')
+        
+        photos_choosed = Choice.objects.filter(liked_by=author.pk)
+        
+        photos = []
+        for i in photos_choosed:
+            photo = Photo.objects.filter(id=i.id)
+            photos.append(photo)
+        kwargs['photos'] = photos
+        print("+++++", kwargs['photos'])
         paginator = Paginator(photos, self.paginate_related_by, orphans=self.paginate_related_orphans)
         page_number = self.request.GET.get('page', 1)
         page = paginator.get_page(page_number)
+        kwargs['page_obj'] = page
+        # kwargs['photos'] = page.object_list
+        kwargs['is_paginated'] = page.has_other_pages()
+        return super().get_context_data(**kwargs, context=context)
+
+
         
         
     # def get_context_data(self, **kwargs):
